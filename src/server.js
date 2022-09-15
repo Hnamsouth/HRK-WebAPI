070cbd8e371007319eed129f517feacf51377806
hoang nam <namhvth2204008@fpt.edu.vn>
2022-09-16 02:39:05 +0700

    1

--- src/server.js
+++ src/server.js
@@ -13,28 +13,29 @@ import 'dotenv/config' //
 
 import bodyParser from 'body-parser';
 import mysql from 'mysql2'
+// const cnnn = mysql.createPool({
+//         host: "pokabi.tech", //  123.16.16.117  - api.pokabi.tech -  pokabi.tech
+//         user: "admin_edit",
+//         password: "adminedit",
+//         database: "eproject",
+//         waitForConnections: true,
+//         connectionLimit: 10,
+//         queueLimit: 0
+//             // port: 3306,
+//             // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+//     })
 const cnnn = mysql.createPool({
-        host: "pokabi.tech", //  123.16.16.117  - api.pokabi.tech -  pokabi.tech
-        user: "admin_edit",
-        password: "adminedit",
-        database: "Eproject",
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0
-            // port: 3306,
-            // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
-    })
-    // const cnnn = mysql.createPool({
-    //     host: "127.0.0.1", //  123.16.16.117  - api.pokabi.tech -  pokabi.tech
-    //     user: "root",
-    //     password: "",
-    //     database: "eproject",
-    //     waitForConnections: true,
-    //     connectionLimit: 10,
-    //     queueLimit: 0
-    //         // port: 3306,
-    //         // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
-    // })
+    host: "127.0.0.1", //  123.16.16.117  - api.pokabi.tech -  pokabi.tech
+    user: "root",
+    password: "",
+    database: "eproject",
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+        // port: 3306,
+        // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+})
 
 
 const app = express()
@@ -56,21 +57,33 @@ app.get("/", async(req, res) => {
     res.status(200).json({ data: rows })
 })
 
-app.get("/checkUer", async(req, res) => {
-    let { Email, Password } = req.query
-    let [rows, fields] = await cnnn.promise().query('select * from register where email like ? and password like ? ', [emailorphone, password])
-    res.status(200).json({ mess: 'User found', status: 1 })
+app.post("/checkUer", async(req, res) => {
+    console.log(req.body)
+    let { emailorphone, password } = req.body
+    if (!emailorphone || !password) {
+        res.status(200).json({ mess: 'Miss required params', status: 404 })
+    } else {
+        let [rows, fields] = await cnnn.promise().query('select count(*) from register where emailorphone like ? and password like ? ', [emailorphone, password])
+        if (rows == 1) {
+            res.status(200).json({ mess: 'User found', status: true, data: rows })
+        } else {
+            res.status(200).json({ mess: 'User not found', status: false, data: rows })
+        }
+    }
 
 })
 app.post("/createUser", async(req, res) => {
+    console.log(req.body)
 
-    let { firstname, lastname, emailorphone, password } = req.query
+    let { firstname, lastname, emailorphone, password } = req.body
         // FirstName,LastName,Email,Password
-    console.log(firstname, lastname, emailorphone, password)
-    if (!firstname || !lastname || !emailorphone || !password) {
-        res.status(200).json({ mess: "Miss required params", params: req.query })
+    if (firstname == '' || lastname == '' || emailorphone == '' || password == '') {
+        res.status(200).json({ mess: "Miss required params", params: req.body })
+    } else {
+        // console.log(firstname, lastname, emailorphone, password)
+        await cnnn.promise().query('INSERT INTO `register` (`firstname`, `lastname`, `emailorphone`, `password`) VALUES (?,?,?,?)', [firstname, lastname, emailorphone, password])
+        res.status(200).json({ mess: "user created", data: req.body })
     }
-    await cnnn.promise().query('insert into register(firstname, lastname, emailorphone, password) values(?,?,?,?)', [firstname, lastname, emailorphone, password])
 })
 
 
